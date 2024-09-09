679bd82a64f10b33eb1ce6efa88a39ea7bee55c1
estiglemanDrift <estigleman@drift.com>
2024-09-09 13:10:01 -0400

    Pot VPN

--- main.js
+++ main.js
@@ -25,7 +25,7 @@
 drift.SNIPPET_VERSION = '0.3.1';
 drift.load('k39mxhti7nmm');
 
-drift.on('ready', function(api){drift.api.startInteraction({ interactionId: 393513 });
+
 
 //drift.on('ready', function(api){drift.api.widget.show(); })	
 
